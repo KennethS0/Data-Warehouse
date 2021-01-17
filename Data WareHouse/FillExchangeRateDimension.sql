400949dfb61b23aa262d1004ba05d011596f9713
josuerv99 <josuerojasvega@gmail.com>
2021-01-17 12:04:32 -0600

    FilExchangeRateDimension: procedure to simplify sql statement for job (sqlagent)

--- Data WareHouse/FillExchangeRateDimension.sql
+++ Data WareHouse/FillExchangeRateDimension.sql
@@ -2,6 +2,44 @@
 USE [ROCKET-BD2]
 GO
 
+CREATE PROCEDURE GetExchangeRateForToday
+AS
+BEGIN
+	 BEGIN TRY
+		DECLARE @lastDate DATE = 
+			( SELECT TOP(1) T.update_date FROM DIM_EXCHANGE_RATE T ORDER BY T.update_date desc )
+
+		while DATEDIFF(day, @lastDate, getDate()) > 0 and 
+			DATEDIFF(MINUTE,GETDATE(),DATEADD(minute, 10, DATEADD(hour, 6, CAST(CAST(GETDATE() AS DATE) AS DATETIME)))) <= 0
+		BEGIN
+
+			DECLARE @lastUpdateDate DATE;
+			DECLARE @PurchaseValue DECIMAL(15, 4), 
+					@SellValue DECIMAL(15, 4);
+		
+			SET @lastDate = DATEADD(day, 1, @lastDate);
+
+			-- call web service
+			EXEC getExchangeRateFromBCCR
+				317, 
+				@lastDate, @lastDate, @lastUpdateDate out, @PurchaseValue out;
+
+			EXEC getExchangeRateFromBCCR
+				318, 
+				@lastDate, @lastDate, @lastUpdateDate out, @SellValue out;
+
+			-- update table with new exchange rate info
+			INSERT INTO DIM_EXCHANGE_RATE(update_date, dollar_purchase_crc, dollar_sell_crc)
+			VALUES(@lastDate, @PurchaseValue, @SellValue);
+		END
+
+	END TRY
+	BEGIN CATCH
+		SELECT ERROR_MESSAGE() AS ErrorMessage;  
+	END CATCH
+END
+
+
 -- filling exchange dimension table with data from BCCR
 CREATE PROCEDURE FillExchangeDimension
 AS
